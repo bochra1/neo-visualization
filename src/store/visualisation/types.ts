dc9b434883f5bc90b1ae52efdb7c35fcc31dde25
bochra <bochralb01@gmail.com>
2025-03-18 15:21:10 +0100

    display as a table with switch btn

--- src/store/visualisation/types.ts
+++ src/store/visualisation/types.ts
@@ -1,8 +1,23 @@
 export interface NearEarthObject {
     id: string;
     name: string;
-    estimated_diameter_min: number;
+    estimated_diameter_min: number ;
     estimated_diameter_max: number;
     estimated_diameter_avg: number;
     orbiting_body: string
+  }
+  export  interface ApiResponse {
+    near_earth_objects: Array<{
+      id: string;
+      name: string;
+      estimated_diameter: {
+        kilometers: {
+          estimated_diameter_min: number;
+          estimated_diameter_max: number;
+        };
+      };
+      close_approach_data?: Array<{
+        orbiting_body: string;
+      }>;
+    }>;
   }
